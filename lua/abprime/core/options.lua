69f163803873b3129af7d30881c3d1d3cfd99064
Abinash Panda <abinashpanda205@gmail.com>
2022-12-12 02:21:26 +0530

    Initial commit

---
+++ lua/abprime/core/options.lua
@@ -0,0 +1,62 @@
+
+local opt = vim.opt
+
+---- global options
+opt.swapfile = true
+opt.dir = '/tmp'
+opt.laststatus = 2
+
+-- appearance
+opt.termguicolors = true
+opt.background = "dark"
+opt.signcolumn = "yes"
+--
+
+-- backspace
+opt.backspace = "indent,eol,start"
+--
+
+-- clipboard
+opt.clipboard:append("unnamedplus")
+--
+
+-- split windows
+opt.splitright = true
+opt.splitbelow = true
+--
+
+-- search settings
+opt.hlsearch = true
+opt.incsearch = true
+opt.ignorecase = true
+opt.smartcase = true
+opt.scrolloff = 12
+--
+
+----
+
+---- window-local options
+opt.wrap = false
+
+-- line numbers
+opt.relativenumber = true
+opt.number = true
+--
+
+-- cursor line
+opt.cursorline = true
+--
+
+----
+
+---- buffer-local options
+opt.iskeyword:append("-")
+
+-- tabs & indentation
+opt.expandtab = true
+opt.tabstop = 4
+opt.shiftwidth = 4
+opt.autoindent = true
+--
+
+----
---
+++ lua/abprime/plugins/lsp/mason.lua
@@ -0,0 +1,46 @@
+local mason_status, mason = pcall(require, "mason")
+if not mason_status then
+    return
+end
+
+local mason_lspconfig_status, mason_lspconfig = pcall(require, "mason-lspconfig")
+if not mason_lspconfig_status then
+    return
+end
+
+local mason_null_ls_status, mason_null_ls = pcall(require, "mason-null-ls")
+if not mason_null_ls_status then
+    return
+end
+
+mason.setup()
+
+mason_lspconfig.setup({
+    ensure_installed = {
+        "pyright",
+        "sourcery",
+        "tsserver",
+        "jsonls",
+        "html",
+        "dockerls",
+        "yamlls",
+        "lemminx",
+        "taplo",
+        "sqlls",
+        "pylsp",
+        "graphql",
+        "sumneko_lua"
+    }
+})
+
+mason_null_ls.setup({
+    ensure_installed = {
+        "prettier",
+        "stylua",
+        "eslint_d",
+        "mypy",
+        "flake8",
+        "isort",
+        "black"
+    }
+})
